6287269b417a0b06446c06c1f488ae5c6627ce54
Haryanzelina Bancin <135719907+haryanzel@users.noreply.github.com>
2023-06-20 21:31:51 +0700

    Add files via upload

---
+++ app/views/index/index.php
@@ -0,0 +1,10 @@
+<h2>Log In</h2>
+
+<div class="info">Selamat datang, <strong>Silahkan LogIN</strong></div>
+<form action="<?php echo URL; ?>/Dashboard/index" method="post">
+    <p>username</p>
+    <input type="text">
+    <p>password</p>
+    <input type="password">
+    <input type="submit" value="Login">
+</form>
---
+++ layouts/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TUGAS 2 WEB | Web Lanjutan</title>
+	<link rel="shortcut icon" href="<?php echo AST; ?>/img/anyabelajr.jpeg" type="image/x-icon">
+	<link rel="stylesheet" href="<?php echo AST; ?>/css/style.css">
+</head>
+
+<body>
+
+	<aside>
+
+		<header>
+			<img src="<?php echo AST; ?>/img/hyanya.jpeg" class="brand">
+			<div class="user">Haryanzelina Bancin</div>
+		</header>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="<?php echo URL; ?>/dashboard">
+						<img class="icon" src="<?php echo AST; ?>/img/anyeremove.png" alt=""> Home
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/user">
+						<img class="icon" src="<?php echo AST; ?>/img/anyeremove.png" alt=""> User
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/golongan">
+						<img class="icon" src="<?php echo AST; ?>/img/anyeremove.png" alt=""> Golongan
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/pelanggan">
+						<img class="icon" src="<?php echo AST; ?>/img/anyeremove.png" alt=""> Pelanggan
+					</a>
+				</li>
+				<li>
+				<a href="<?php echo URL; ?>/index">
+					<img id="logout" class="icon" src="<?php echo AST; ?>/img/anyeremove.png" alt=""> LogOut
+				</a>
+				</li>
+			</ul>
+		</nav>
+
+	</aside>
+
+	<main>
+		<article>
+			<?php require_once ROOT . "app/views/" . $view . ".php"; ?>
+		</article>
+
+		<footer>
+			Copyright &copy; 2023. Designed by Haryanzelina Bancin
+		</footer>
+	</main>
+
+</body>
+
+</html>
